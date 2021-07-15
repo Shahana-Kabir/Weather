f82225e038603ffcfcfa340628355b5a925b7f68
Shahana Kabir <shahana.kabir@gmail.com>
2021-07-15 12:31:50 -0700

    modified

--- app.js
+++ app.js
@@ -4,21 +4,19 @@ const app = express();
 const https = require("https");
 
 app.get("/", function(req,res){
-    res.send("server is up and running");
+    
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Coquitlam&appid=dac3fe55cc61310aa8cac1d25538506f#"
     https.get(url, function(response){
         console.log(response.statusCode);
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
             console.log(weatherData);
             console.log(temp);
-            const object = {
-                name: "Shanu",
-                fav: "rice"
-            }
-            JSON.stringify(object);
-            console.log(object);
+            res.write ("<p>temperature description is " + weatherDescription + "</p>");
+            res.write("<h1>the temperature in London is" + temp + "degree celcius. </h1>")
+            res.send();
         })
     })
 })
